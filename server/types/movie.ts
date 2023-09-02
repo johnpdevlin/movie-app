a2f49ddb580b75f8d5921c69bd6a627504fd12e2
John Devlin <johnp.devlin2@gmail.com>
2023-09-02 16:01:29 +0100

    create movieInfo and movieDetails types to annotate api response

---
+++ server/types/movie.ts
@@ -0,0 +1,60 @@
+/** @format */
+
+export type MovieInfo = {
+	adult: boolean;
+	backdrop_path: null | string;
+	genre_ids: number[];
+	id: number;
+	original_language: string;
+	original_title: string;
+	overview: string;
+	popularity: number;
+	poster_path: string;
+	release_date: string;
+	title: string;
+	video: boolean;
+	vote_average: number;
+	vote_count: number;
+};
+
+export type MovieDetails = {
+	adult: boolean;
+	backdrop_path: string | null;
+	belongs_to_collection: null | {
+		id: number;
+		name: string;
+	};
+	budget: number;
+	genres: {
+		id: number;
+		name: string;
+	}[];
+	homepage: string;
+	id: number;
+	imdb_id: string;
+	original_language: string;
+	original_title: string;
+	overview: string;
+	popularity: number;
+	poster_path: string | null;
+	production_companies: {
+		name: string;
+	}[];
+	production_countries: {
+		iso_3166_1: string;
+		name: string;
+	}[];
+	release_date: string;
+	revenue: number;
+	runtime: number;
+	spoken_languages: {
+		iso_639_1: string;
+		name: string;
+	}[];
+	status: string;
+	tagline: string;
+	title: string;
+	video: boolean;
+	vote_average: number;
+	vote_count: number;
+};
