c38edf5beb53146ee47c5a65c85681d1688c778e
John Devlin <johnp.devlin2@gmail.com>
2023-09-02 16:04:03 +0100

    add axios fetch routes, limiters and error handling

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,46 +1,90 @@
 "use strict";
 /** @format */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 const express_1 = __importDefault(require("express"));
 const node_cache_1 = __importDefault(require("node-cache"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const port = 8000;
 const app = (0, express_1.default)();
 const cache = new node_cache_1.default({ stdTTL: 600 }); // Cache results for 10 minutes
-app.get('/search/:title', (req, res) => {
+// Define rate limits for different routes
+const searchRateLimiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 50, // Limit to 50 requests per IP for /search route
+});
+const moviesRateLimiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 100, // Limit to 100 requests per IP for /movies route
+});
+app.use(express_1.default.json());
+// Apply rate limiters to specific routes
+app.use('/search/:title', searchRateLimiter);
+app.use('/movie/:movieId', moviesRateLimiter);
+const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTk1ZGE4NTQ3NGM0MjY0ZjU0MjRlMjlkN2JkOGY4NSIsInN1YiI6IjY0ZjMzY2NlM2Q0M2UwMDBmZTI3NGVmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lLzleyQ8W6nh1dhUJsDkCDh4PP2Ng2udBgU93PDesbA';
+const options = {
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+    },
+};
+// Gets movies with similar titles
+app.get('/search/:title', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const title = req.params.title;
-    const cacheKey = `s-${title}`;
-    const response = {
-        data: {
-            hello: title,
-        },
-    };
-    // Check cache first
-    const cachedResult = cache.get(cacheKey);
-    if (cachedResult) {
-        return res.json(cachedResult);
+    try {
+        const page = req.query.page || 1;
+        const cacheKey = `s-${title}`;
+        const url = `https://api.themoviedb.org/3/search/movie?query=${title}&language=en-US&page=${page}`;
+        const response = yield axios_1.default.get(url, options);
+        // Check cache first
+        const cachedResult = cache.get(cacheKey);
+        if (cachedResult) {
+            return res.json(cachedResult);
+        }
+        cache.set(cacheKey, response.data.results);
+        res.json(response.data.results);
     }
-    cache.set(cacheKey, response.data);
-    return res.json(response.data);
-});
-app.get('/movie/:movieId', (req, res) => {
+    catch (error) {
+        console.error('Error fetching search results:', error);
+        res
+            .status(500)
+            .json({ error: `Error fetching search results for ${title}` });
+    }
+}));
+// Gets individual movie by id
+app.get('/movie/:movieId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const movieId = req.params.movieId;
-    const cacheKey = `m-${movieId}`;
-    const response = {
-        data: {
-            hello: movieId,
-        },
-    };
-    // Check cache first
-    const cachedResult = cache.get(cacheKey);
-    if (cachedResult) {
-        return res.json(cachedResult);
+    try {
+        const cacheKey = `m-${movieId}`;
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
+        const response = yield axios_1.default.get(url, options);
+        // Check cache first
+        const cachedResult = cache.get(cacheKey);
+        if (cachedResult) {
+            return res.json(cachedResult);
+        }
+        cache.set(cacheKey, response.data);
+        res.json(response.data);
     }
-    cache.set(cacheKey, response.data);
-    return res.json(response.data);
-});
+    catch (error) {
+        console.error(`Error fetching movie details for: ${movieId} `, error);
+        res.status(500).json({
+            error: `Incorrect MovieId (${movieId}) submitted or server error.`,
+        });
+    }
+}));
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,48 +1,93 @@
 /** @format */
 
-import express from 'express';
+import axios from 'axios';
+import express, { Request, Response } from 'express';
 import NodeCache from 'node-cache';
+import rateLimit from 'express-rate-limit';
+import { MovieDetails, MovieInfo } from './types/movie';
 
 const port = 8000;
 const app = express();
 
 const cache = new NodeCache({ stdTTL: 600 }); // Cache results for 10 minutes
 
-app.get('/search/:title', (req, res) => {
-	const title = req.params.title;
-	const cacheKey = `s-${title}`;
-	const response = {
-		data: {
-			hello: title,
-		},
-	};
-	// Check cache first
-	const cachedResult = cache.get(cacheKey);
-	if (cachedResult) {
-		return res.json(cachedResult);
-	}
-
-	cache.set(cacheKey, response.data);
-	return res.json(response.data);
+// Define rate limits for different routes
+const searchRateLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 50, // Limit to 50 requests per IP for /search route
 });
 
-app.get('/movie/:movieId', (req, res) => {
-	const movieId = req.params.movieId;
+const moviesRateLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit to 100 requests per IP for /movies route
+});
 
-	const cacheKey = `m-${movieId}`;
-	const response = {
-		data: {
-			hello: movieId,
-		},
-	};
-	// Check cache first
-	const cachedResult = cache.get(cacheKey);
-	if (cachedResult) {
-		return res.json(cachedResult);
+app.use(express.json());
+
+// Apply rate limiters to specific routes
+app.use('/search/:title', searchRateLimiter);
+app.use('/movie/:movieId', moviesRateLimiter);
+
+const apiKey =
+	'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTk1ZGE4NTQ3NGM0MjY0ZjU0MjRlMjlkN2JkOGY4NSIsInN1YiI6IjY0ZjMzY2NlM2Q0M2UwMDBmZTI3NGVmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lLzleyQ8W6nh1dhUJsDkCDh4PP2Ng2udBgU93PDesbA';
+
+const options = {
+	headers: {
+		accept: 'application/json',
+		Authorization: `Bearer ${apiKey}`,
+	},
+};
+
+// Gets movies with similar titles
+app.get('/search/:title', async (req: Request, res: Response) => {
+	const title = req.params.title;
+	try {
+		const page = req.query.page || 1;
+		const cacheKey = `s-${title}`;
+
+		const url = `https://api.themoviedb.org/3/search/movie?query=${title}&language=en-US&page=${page}`;
+
+		const response = await axios.get(url, options);
+
+		// Check cache first
+		const cachedResult = cache.get(cacheKey);
+		if (cachedResult) {
+			return res.json(cachedResult as MovieInfo[]);
+		}
+
+		cache.set(cacheKey, response.data.results);
+		res.json(response.data.results);
+	} catch (error) {
+		console.error('Error fetching search results:', error);
+		res
+			.status(500)
+			.json({ error: `Error fetching search results for ${title}` });
 	}
+});
 
-	cache.set(cacheKey, response.data);
-	return res.json(response.data);
+// Gets individual movie by id
+app.get('/movie/:movieId', async (req: Request, res: Response) => {
+	const movieId = req.params.movieId;
+	try {
+		const cacheKey = `m-${movieId}`;
+		const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
+
+		const response = await axios.get(url, options);
+
+		// Check cache first
+		const cachedResult: MovieDetails | undefined = cache.get(cacheKey);
+		if (cachedResult) {
+			return res.json(cachedResult as MovieDetails);
+		}
+
+		cache.set(cacheKey, response.data);
+		res.json(response.data);
+	} catch (error) {
+		console.error(`Error fetching movie details for: ${movieId} `, error);
+		res.status(500).json({
+			error: `Incorrect MovieId (${movieId}) submitted or server error.`,
+		});
+	}
 });
 
 app.listen(port, () => {
