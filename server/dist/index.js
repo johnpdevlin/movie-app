262693503b54866f2047b177fbb0ab1d7e2fa950
John Devlin <johnp.devlin2@gmail.com>
2023-09-02 14:16:49 +0100

    set up basic express server + cache functionality

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,11 +5,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const node_cache_1 = __importDefault(require("node-cache"));
 const port = 8000;
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+const cache = new node_cache_1.default({ stdTTL: 600 }); // Cache results for 10 minutes
+app.get('/search/:title', (req, res) => {
+    const title = req.params.title;
+    const cacheKey = `s-${title}`;
+    const response = {
+        data: {
+            hello: title,
+        },
+    };
+    // Check cache first
+    const cachedResult = cache.get(cacheKey);
+    if (cachedResult) {
+        return res.json(cachedResult);
+    }
+    cache.set(cacheKey, response.data);
+    return res.json(response.data);
+});
+app.get('/movie/:movieId', (req, res) => {
+    const movieId = req.params.movieId;
+    const cacheKey = `m-${movieId}`;
+    const response = {
+        data: {
+            hello: movieId,
+        },
+    };
+    // Check cache first
+    const cachedResult = cache.get(cacheKey);
+    if (cachedResult) {
+        return res.json(cachedResult);
+    }
+    cache.set(cacheKey, response.data);
+    return res.json(response.data);
 });
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}.`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,50 @@
 /** @format */
 
 import express from 'express';
+import NodeCache from 'node-cache';
 
 const port = 8000;
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+const cache = new NodeCache({ stdTTL: 600 }); // Cache results for 10 minutes
+
+app.get('/search/:title', (req, res) => {
+	const title = req.params.title;
+	const cacheKey = `s-${title}`;
+	const response = {
+		data: {
+			hello: title,
+		},
+	};
+	// Check cache first
+	const cachedResult = cache.get(cacheKey);
+	if (cachedResult) {
+		return res.json(cachedResult);
+	}
+
+	cache.set(cacheKey, response.data);
+	return res.json(response.data);
+});
+
+app.get('/movie/:movieId', (req, res) => {
+	const movieId = req.params.movieId;
+
+	const cacheKey = `m-${movieId}`;
+	const response = {
+		data: {
+			hello: movieId,
+		},
+	};
+	// Check cache first
+	const cachedResult = cache.get(cacheKey);
+	if (cachedResult) {
+		return res.json(cachedResult);
+	}
+
+	cache.set(cacheKey, response.data);
+	return res.json(response.data);
 });
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}.`);
 });
