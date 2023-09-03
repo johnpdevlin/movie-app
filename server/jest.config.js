be6a51006b87d4f9349560e3032fe17e4f926c08
John Devlin <johnp.devlin2@gmail.com>
2023-09-03 13:17:36 +0100

    install and config jest and supertest

---
+++ server/jest.config.js
@@ -0,0 +1,9 @@
+/** @format */
+
+// jest.config.js
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	setupFiles: ['./jest.setup.js'],
+};
---
+++ server/jest.setup.js
@@ -0,0 +1,7 @@
+/** @format */
+
+// jest.setup.js
+const dotenv = require('dotenv');
+
+// Load environment variables from .env.test
+dotenv.config({ path: '.env.test' });
