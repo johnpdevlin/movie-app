f8df753fd59ad24dc9ce23ab292742bf9a096206
John Devlin <johnp.devlin2@gmail.com>
2023-09-06 12:04:11 +0100

    create rating ui component

---
+++ client/src/UI-Components/MovieRating.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+
+export default function MovieRating(props: { value: number }) {
+	return (
+		<>
+			<Stack direction='row' alignItems='center'>
+				<Rating defaultValue={props.value} precision={0.1} readOnly />
+				<small>({props.value})</small>
+			</Stack>
+		</>
+	);
+}
