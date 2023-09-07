5de3c51213f57739a58b3ef4e0aec6eb3e5f0a23
John Devlin <johnp.devlin2@gmail.com>
2023-09-07 16:13:37 +0100

    create rating component

--- client/src/UI-Components/MovieRating.tsx
+++ client/src/UI-Components/MovieRating.tsx
@@ -8,7 +8,7 @@ export default function MovieRating(props: { value: number }) {
 		<>
 			<Stack direction='row' alignItems='center'>
 				<Rating defaultValue={props.value} precision={0.1} readOnly />
-				<small>({props.value})</small>
+				<small>({props.value.toFixed(1)})</small>
 			</Stack>
 		</>
 	);
