d33641dd37f016c462aefb2cc752ab451bb5bce6
John Devlin <johnp.devlin2@gmail.com>
2023-09-08 01:33:50 +0100

    create react context for favorites

---
+++ client/src/context/favoritesContext.tsx
@@ -0,0 +1,65 @@
+/** @format */
+
+import { createContext, useContext, ReactNode } from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage'; // Import your useLocalStorage hook
+import { MovieDetails } from '../../models/movie'; // Import your MovieDetails model
+
+// Create a context
+type FavoriteMoviesContextType = {
+	favoriteMovies: MovieDetails[];
+	addFavoriteMovie: (movie: MovieDetails) => void;
+	removeFavoriteMovie: (movieId: number) => void;
+	getFavoritesCount: () => number; // Add getFavoritesCount function
+};
+
+const FavoriteMoviesContext = createContext<
+	FavoriteMoviesContextType | undefined
+>(undefined);
+
+type FavoriteMoviesProviderProps = {
+	children: ReactNode;
+};
+
+export function useFavoriteMovies() {
+	const context = useContext(FavoriteMoviesContext);
+	if (!context) {
+		throw new Error(
+			'useFavoriteMovies must be used within a FavoriteMoviesProvider'
+		);
+	}
+	return context;
+}
+
+export function FavoriteMoviesProvider({
+	children,
+}: FavoriteMoviesProviderProps) {
+	const [favoriteMovies, setFavoriteMovies] = useLocalStorage<MovieDetails[]>(
+		'favoriteMovies',
+		[]
+	);
+
+	const addFavoriteMovie = (movie: MovieDetails) => {
+		setFavoriteMovies((prevFavorites) => [...prevFavorites, movie]);
+	};
+
+	const removeFavoriteMovie = (movieId: number) => {
+		setFavoriteMovies((prevFavorites) =>
+			prevFavorites.filter((movie) => movie.id !== movieId)
+		);
+	};
+
+	const getFavoritesCount = () => favoriteMovies.length; // Implement getFavoritesCount
+
+	const contextValue: FavoriteMoviesContextType = {
+		favoriteMovies,
+		addFavoriteMovie,
+		removeFavoriteMovie,
+		getFavoritesCount, // Include getFavoritesCount in the context
+	};
+
+	return (
+		<FavoriteMoviesContext.Provider value={contextValue}>
+			{children}
+		</FavoriteMoviesContext.Provider>
+	);
+}
