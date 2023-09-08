77561170806482fc9d6a87fdb1b7182e60da092f
John Devlin <johnp.devlin2@gmail.com>
2023-09-08 01:36:16 +0100

    add proper loading states

--- client/src/UI-Components/MovieGrid.tsx
+++ client/src/UI-Components/MovieGrid.tsx
@@ -2,35 +2,49 @@
 
 import Grid from '@mui/material/Grid';
 import MovieCompact from './MovieCompact';
-import { MovieInfo } from '../../models/movie';
+import Skeleton from '@mui/material/Skeleton';
+import { MovieDetails, MovieInfo } from '../../models/movie';
 
-// import MovieCompact from './MovieCompact';
-
-const MovieGrid = (props: { isLoading: Boolean; data: MovieInfo[] }) => {
+function MovieGrid(props: {
+	isLoading: Boolean;
+	data: (MovieInfo | MovieDetails)[];
+}) {
 	return (
 		<>
 			{props.isLoading ? (
-				<p>Loading...</p>
-			) : (
-				<Grid container gap={1.5}>
-					{props.data.map((info: MovieInfo, index) => {
+				<Grid container mt={3} gap={1.5} justifyContent='flex'>
+					{Array.from(new Array(20)).map((_, index) => {
 						return (
 							<>
-								<Grid item xs={6} sm={4} md={3} key={index}>
-									<MovieCompact
-										id={info.id}
-										poster={info.poster_path}
-										title={info.title}
-										release_date={info.release_date}
-									/>
+								<Grid item xs={3.6} sm={2.8} md={1.8} lg={1.6} key={index}>
+									<Skeleton variant='rectangular' width='100%' height={300} />
 								</Grid>
 							</>
 						);
 					})}
 				</Grid>
+			) : (
+				<>
+					<Grid container mt={3} gap={1.5} justifyContent='flex'>
+						{props.data.map((info: MovieInfo | MovieDetails, index) => {
+							return (
+								<>
+									<Grid item xs={3.6} sm={2.8} md={1.8} lg={1.6} key={index}>
+										<MovieCompact
+											id={info.id}
+											poster={info.poster_path!}
+											title={info.title}
+											release_date={info.release_date}
+											key={index}
+										/>
+									</Grid>
+								</>
+							);
+						})}
+					</Grid>
+				</>
 			)}
 		</>
 	);
-};
-
+}
 export default MovieGrid;
