7e1bca5128b8ee8a2cbda26abd74ef8feb428d92
John Devlin <johnp.devlin2@gmail.com>
2023-09-08 01:33:17 +0100

    create routes and top level logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @format */
+
+import { useEffect, useState } from 'react';
+import AppBar from './UI-Components/AppBar';
+import MovieGrid from './UI-Components/MovieGrid';
+import { MovieDetails, MovieInfo } from '../models/movie';
+import axios from 'axios';
+import MoviePage from './UI-Components/MoviePage';
+import FavoritesPage from './UI-Components/FavoritesPage';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { FavoriteMoviesProvider } from './context/favoritesContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [searchTerm, setSearchTerm] = useState<string>('');
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState<Array<MovieDetails | MovieInfo>>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		if (searchTerm !== '') {
+			setIsLoading(true);
+
+			axios
+				.get(`http://localhost:8000/search/${searchTerm}`)
+
+				.then((res) => {
+					console.log(res);
+					const filteredData = res.data.results.filter(
+						(res: MovieInfo) => res.poster_path !== null
+					) as MovieInfo[];
+
+					// Check for duplicates before adding to the data
+
+					setData(filteredData);
+				})
+				.finally(() => {
+					setIsLoading(false);
+				});
+		}
+	}, [searchTerm]);
+
+	return (
+		<>
+			<FavoriteMoviesProvider>
+				<AppBar setSearchTerm={setSearchTerm} />
+				<Routes>
+					<Route
+						path='/'
+						element={<MovieGrid isLoading={isLoading} data={data} />}
+					/>
+					<Route path='/:id' element={<MoviePage />} />
+					<Route path='/favorites' element={<FavoritesPage />} />
+					<Route path='*' element={<Navigate to='/' />} />
+				</Routes>
+			</FavoriteMoviesProvider>
+		</>
+	);
 }
 
-export default App
+export default App;
