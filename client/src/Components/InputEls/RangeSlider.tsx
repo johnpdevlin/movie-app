c03df998c4e25a744d84b8df482e81e82bdba65c
John Devlin <johnp.devlin2@gmail.com>
2023-09-10 21:05:22 +0100

    create slider el for ranges

---
+++ client/src/Components/InputEls/RangeSlider.tsx
@@ -0,0 +1,41 @@
+/** @format */
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { formatMinutesToHoursAndMinutes } from '../../utils/format';
+
+function formatRuntime(value: number) {
+	return formatMinutesToHoursAndMinutes(value);
+}
+
+type RangeSliderProps = {
+	label: string;
+	type: 'year' | 'runtime';
+	range: [number, number];
+	setRange: React.Dispatch<React.SetStateAction<[number, number]>>;
+};
+
+export default function RangeSlider(props: RangeSliderProps) {
+	const handleChange = (_event: Event, newValue: number | number[]) => {
+		props.setRange(newValue as [number, number]);
+	};
+	let min: number = 0;
+	let max: number = 100;
+
+	if (props.type === 'year') [min, max] = [1880, new Date().getFullYear()];
+	else if (props.type === 'runtime') [min, max] = [0, 360];
+
+	return (
+		<Box>
+			<Slider
+				getAriaLabel={() => props.label}
+				value={props.range}
+				onChange={handleChange}
+				valueLabelDisplay='auto'
+				min={min}
+				max={max}
+			/>
+		</Box>
+	);
+}
